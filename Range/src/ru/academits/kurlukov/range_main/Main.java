08a4d9393c99b2d1536d80e6e434354e18f57aad
Пользователь <kuralex92@mail.ru>
2023-07-16 12:52:34 +0700

    Add Range project

--- Range/src/ru/academits/kurlukov/range_main/Main.java
+++ Range/src/ru/academits/kurlukov/range_main/Main.java
@@ -1,7 +1,29 @@
 package ru.academits.kurlukov.range_main;
 
+import ru.academits.kurlukov.range.Range;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Range range = new Range(0, 12);
+        System.out.println("Длина диапазона равна: " + range.getLength());
+
+        range.setTo(15);
+        range.setFrom(-1);
+
+        double to = range.getTo();
+        System.out.println(to);
+
+        double from = range.getFrom();
+        System.out.println(from);
+
+        System.out.println("Длина диапазона равна: " + range.getLength());
+
+        double number = 5;
+
+        if (range.isInside(number)) {
+            System.out.println("Число " + number + " принадлежит диапазону");
+        } else {
+            System.out.println("Число " + number + " не принадлежит диапазону");
+        }
     }
 }

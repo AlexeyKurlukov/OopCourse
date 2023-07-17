a34c39a67667f4d550d7af51ce3033671a8ef4ea
Пользователь <kuralex92@mail.ru>
2023-07-17 17:18:23 +0700

    Add Range*(part2) project

--- Range/src/ru/academits/kurlukov/range/Range.java
+++ Range/src/ru/academits/kurlukov/range/Range.java
@@ -30,6 +30,55 @@ public class Range {
     }
 
     public boolean isInside(double number) {
-        return number - from >= 0 && to - number >= 0;
+        return number >= from && number <= to;
+    }
+
+    public Range getIntersection(Range range) {
+        double minTo = Math.min(this.to, range.to);
+        double maxFrom = Math.max(this.from, range.from);
+
+        if (minTo <= maxFrom) {
+            return null;
+        }
+
+        return new Range(maxFrom, minTo);
+    }
+
+    public Range[] getUnion(Range range) {
+        double minFrom = Math.min(this.from, range.from);
+        double maxTo = Math.max(this.to, range.to);
+
+        if (minFrom == this.from && maxTo == this.to) {
+            return new Range[]{this};
+        }
+
+        if (minFrom == range.from && maxTo == range.to) {
+            return new Range[]{range};
+        }
+
+        return new Range[]{new Range(minFrom, maxTo)};
+    }
+
+    public Range[] getDifference(Range range) {
+        if (this.from >= range.to || this.to <= range.from) {
+            return new Range[]{this};
+        }
+
+        if (this.from >= range.from && this.to <= range.to) {
+            return new Range[]{};
+        }
+
+        if (this.from < range.from && this.to > range.to) {
+            return new Range[]{
+                    new Range(this.from, range.from),
+                    new Range(range.to, this.to)
+            };
+        }
+
+        if (this.from < range.from) {
+            return new Range[]{new Range(this.from, range.from)};
+        }
+
+        return new Range[]{new Range(range.to, this.to)};
     }
 }
--- Range/src/ru/academits/kurlukov/range_main/Main.java
+++ Range/src/ru/academits/kurlukov/range_main/Main.java
@@ -2,28 +2,52 @@ package ru.academits.kurlukov.range_main;
 
 import ru.academits.kurlukov.range.Range;
 
+
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(0, 12);
-        System.out.println("Длина диапазона равна: " + range.getLength());
+        Range range1 = new Range(0, 12);
+        System.out.println("Длина диапазона равна: " + range1.getLength());
 
-        range.setTo(15);
-        range.setFrom(-1);
+        range1.setTo(15);
+        range1.setFrom(-1);
 
-        double to = range.getTo();
+        double to = range1.getTo();
         System.out.println(to);
 
-        double from = range.getFrom();
+        double from = range1.getFrom();
         System.out.println(from);
 
-        System.out.println("Длина диапазона равна: " + range.getLength());
+        System.out.println("Длина диапазона равна: " + range1.getLength());
 
         double number = 5;
 
-        if (range.isInside(number)) {
+        if (range1.isInside(number)) {
             System.out.println("Число " + number + " принадлежит диапазону");
         } else {
             System.out.println("Число " + number + " не принадлежит диапазону");
+        }
+
+        Range range2 = new Range(4, 9);
+        Range range3 = new Range(7, 8);
+
+        Range intersection = range2.getIntersection(range3);
+
+        if (intersection == null) {
+            System.out.println("Пересечения нет");
+        } else {
+            System.out.println("Пересечение двух интервалов: от " + intersection.getFrom() + " до " + intersection.getTo());
+        }
+
+        Range[] union = range2.getUnion(range3);
+
+        for (Range range : union) {
+            System.out.println("Объединение двух интервалов: от " + range.getFrom() + " до " + range.getTo());
+        }
+
+        Range[] difference = range2.getDifference(range3);
+
+        for (Range range : difference) {
+            System.out.println("Разность двух интервалов: от " + range.getFrom() + " до " + range.getTo());
         }
     }
 }

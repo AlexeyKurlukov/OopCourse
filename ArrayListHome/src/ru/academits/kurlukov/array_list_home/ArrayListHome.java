443b52dfdb4a540a2dce1e41bbe3eb7b077d55e7
Пользователь <kuralex92@mail.ru>
2023-08-30 12:02:55 +0700

    Fixed bugs in ArrayListHome project

--- ArrayListHome/src/ru/academits/kurlukov/array_list_home/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/kurlukov/array_list_home/ArrayListHome.java
@@ -5,11 +5,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.io.FileNotFoundException;
 
 public class ArrayListHome {
-    public static List<String> readLinesFromFile(String fileName) {
+    public static List<String> readLinesFromFile(String fileName) throws IOException {
         List<String> lines = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
@@ -18,46 +19,43 @@ public class ArrayListHome {
             while ((line = reader.readLine()) != null) {
                 lines.add(line);
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("Файл не найден");
-        } catch (IOException e) {
-            System.out.println("Ошибка при чтении файла");
         }
 
         return lines;
     }
 
-    public static List<Integer> removeEvenNumbers(List<Integer> numbers) {
-        List<Integer> oddNumbers = new ArrayList<>();
+    public static void removeEvenNumbers(List<Integer> numbers) {
+        Iterator<Integer> iterator = numbers.iterator();
 
-        for (Integer number : numbers) {
-            if (number % 2 != 0) {
-                oddNumbers.add(number);
+        while (iterator.hasNext()) {
+            Integer number = iterator.next();
+
+            if (number % 2 == 0) {
+                iterator.remove();
             }
         }
-
-        return oddNumbers;
     }
 
-    public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> originalList) {
-        ArrayList<Integer> resultList = new ArrayList<>();
+    public static ArrayList<Integer> getUniqueElements(ArrayList<Integer> numbers) {
+        ArrayList<Integer> uniqueElementsList = new ArrayList<>(numbers.size());
 
-        for (Integer number : originalList) {
-            if (!resultList.contains(number)) {
-                resultList.add(number);
+        for (Integer number : numbers) {
+            if (!uniqueElementsList.contains(number)) {
+                uniqueElementsList.add(number);
             }
         }
 
-        return resultList;
+        return uniqueElementsList;
     }
 
     public static void main(String[] args) {
-        List<String> lines = readLinesFromFile("file.txt");
-
-        if (lines.isEmpty()) {
-            System.out.println("Файл пуст или не найден");
-        } else {
+        try {
+            List<String> lines = readLinesFromFile("file.txt");
             System.out.println("В файле содержатся следующие строки: " + lines);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла");
         }
 
         ArrayList<Integer> numbers = new ArrayList<>();
@@ -66,13 +64,13 @@ public class ArrayListHome {
         numbers.addAll(Arrays.asList(numbersArray));
         System.out.println("Список до удаления чётных чисел: " + numbers);
 
-        List<Integer> oddNumbers = removeEvenNumbers(numbers);
-        System.out.println("Список после удаления чётных чисел: " + oddNumbers);
+        removeEvenNumbers(numbers);
+        System.out.println("Список после удаления чётных чисел: " + numbers);
 
         ArrayList<Integer> originalList = new ArrayList<>(Arrays.asList(numbersArray));
         System.out.println("Список с повторяющимися числами: " + originalList);
 
-        ArrayList<Integer> resultList = removeDuplicates(originalList);
-        System.out.println("Список без повторяющихся чисел: " + resultList);
+        ArrayList<Integer> uniqueElementsList = getUniqueElements(originalList);
+        System.out.println("Список без повторяющихся чисел: " + uniqueElementsList);
     }
 }
